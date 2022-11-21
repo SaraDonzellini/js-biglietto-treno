cca2a73c13df97fed7610e3b292eca9286f77412
SaraDonzellini <saradonzie@gmail.com>
2022-11-21 13:49:47 +0100

    starting exercise

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const numberOfKm = parseInt(prompt("Quanti chilometri devi percorrere?"));
+const passengerAge = parseInt(prompt("Quanti anni hai?"));
+
+if (passengerAge <= 20) {
+  
+} else {
+  
+}
+
