273e39bf04462d403d653f4629880464acc0b9c8
SaraDonzellini <saradonzie@gmail.com>
2022-11-21 14:18:11 +0100

    exercise complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
-const numberOfKm = parseInt(prompt("Quanti chilometri devi percorrere?"));
+const numberOfKm = parseFloat(prompt("Quanti chilometri devi percorrere?"));
 const passengerAge = parseInt(prompt("Quanti anni hai?"));
 
+
+document.getElementById('km').innerHTML = numberOfKm + " Km";
+document.getElementById('età').innerHTML = passengerAge + " anni";
+
+
+let ticketFull = numberOfKm * 0.21;
+
+console.log(ticketFull);
+
+let ticketTwenty = ticketFull - (ticketFull * 0.20);
+let ticketSixtyFive = ticketFull - (ticketFull * 0.40);
+
+console.log(ticketTwenty);
+console.log(ticketSixtyFive);
+
+
 if (passengerAge <= 20) {
-  
+
+  document.getElementById('prezzo').innerHTML = 'Il prezzo è: ' + ticketTwenty.toFixed(2);
+
+} else if (passengerAge >= 65) {
+
+  document.getElementById('prezzo').innerHTML = 'Il prezzo è: ' + ticketSixtyFive.toFixed(2);
+
 } else {
   
-}
+  document.getElementById('prezzo').innerHTML = 'Il prezzo è: ' + ticketFull.toFixed(2);
+
+};
 
